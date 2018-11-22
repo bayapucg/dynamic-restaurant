653c2bd93e87d6fe80d07a4c365c76b578ce8166
manchalajahnavi <jahnavimanchala89@gmail.com>
2018-11-22 12:40:56 +0530

    commit

--- admin/contactus.php
+++ admin/contactus.php
@@ -15,13 +15,11 @@
 					<form id="defaultForm" method="post" class="m-b-30" action="contactus.php">
 					
 								<div class="row"> 
-								<div class="col-lg-12">
-									<h4 class="text-primary">Contact us form Email</h4>
-								</div>
+								
 									<div class="col-md-6">
 										<div class="form-group">
-											<label>Email </label>
-											<input type="text" class="form-control" placeholder="Enter Email id">
+											<label>Banner </label>
+											<input type="file" class="form-control" >
 										</div>
 										
 										
@@ -77,6 +75,19 @@
 									</div>
 									
 								</div>
+								<div class="row"> 
+								<div class="col-lg-12">
+									<h4 class="text-primary">Contact us form Email</h4>
+								</div>
+									<div class="col-md-6">
+										<div class="form-group">
+											<label>Email </label>
+											<input type="text" class="form-control" placeholder="Enter Email id">
+										</div>
+										
+										
+									</div>
+									</div>
 								<div class="row"> 
 								<div class="col-lg-12">
 								<h4 class="text-primary">Contact us </h4>
